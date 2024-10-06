45873932d560dd8a2ade04e4119a96847c848a22
variniase <variniaserrallisonuun2iflw@hotmail.com>
2024-10-06 08:39:58 +0000

    Initialised

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("8a6ca91afecd84bb500ca949cab71924d2a49bb5ab47b2e60df36631d8a585ba","8a6ca91afecd84bb500ca949cab71924d2a49bb5ab47b2e60df36631d8a585ba"){} 
+    constructor()ERC20("a52fa9efb7d9b638ad8de9b9092812ddf9afe0052515f24d97b5560afc453529","a52fa9efb7d9b638ad8de9b9092812ddf9afe0052515f24d97b5560afc453529"){} 
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
